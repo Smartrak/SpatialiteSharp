af1c6e9dcb15fb0e3480062fc3fe6b8eac9b642b
Alexander Lagun <alexander.lagun@smartrak.com>
2021-05-21 14:57:27 +1000

    [PS-936]: Add script to copy x86 and x64 files to output directory during the build

--- build/build.bat
+++ build/build.bat
@@ -1,12 +1,12 @@
 msbuild ..\SpatialiteSharp.sln /t:Rebuild /p:Configuration=Release
 
-mkdir content\x64\spatialite
-mkdir content\x86\spatialite
-mkdir content
+mkdir files\x64\spatialite
+mkdir files\x86\spatialite
+mkdir files
 mkdir lib
 
-copy ..\SpatialiteSharp\x64\spatialite content\x64\spatialite
-copy ..\SpatialiteSharp\x86\spatialite content\x86\spatialite
+copy ..\SpatialiteSharp\x64\spatialite files\x64\spatialite
+copy ..\SpatialiteSharp\x86\spatialite files\x86\spatialite
 
 copy ..\SpatialiteSharp\bin\Release\SpatialiteSharp.dll lib
 copy ..\SpatialiteSharp\bin\Release\SpatialiteSharp.xml lib
